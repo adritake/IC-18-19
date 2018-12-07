f94fb6c7249154f74b3ee15539669cd682be2e1a
Adrián de la Torre Rodríguez <atr96@correo.ugr.es>
2018-12-07 14:02:54 +0100

    Arreglo tildes en codigo y añado memoria

--- MNIST/src/practica1/Main.java
+++ MNIST/src/practica1/Main.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		/*
-		//Lectura de las im·genes de entrenamiento
+		//Lectura de las im√°genes de entrenamiento
 		int [][][] imagenes = null;
 		try {
 			imagenes = MNISTDatabase.readImages("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\train-images-idx3-ubyte.gz");
@@ -15,10 +15,10 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Cantidad de im·genes: " + String.valueOf(imagenes.length));
-		
+		System.out.println("Cantidad de im√°genes: " + String.valueOf(imagenes.length));
+
 		/*
-		//Codigo para pintar el primer n˙mero por la salida
+		//Codigo para pintar el primer n√∫mero por la salida
 		String line;
 		for(int i = 0; i < 28; i++) {
 			line = "";
@@ -28,37 +28,37 @@ public class Main {
 			System.out.println(line);
 		}
 		*/
-		
-		
-		
+
+
+
 		/*
 		//Lectura de las etiquetas de entrenamiento
 		int [] labels = null;
-		
+
 		try {
 			labels = MNISTDatabase.readLabels("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\train-labels-idx1-ubyte.gz");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Cantidad de etiquetas: " + String.valueOf(labels.length));
-		
+
 		/*
-		//CÛdigo para imprimir las diez primeras etiquetas
+		//C√≥digo para imprimir las diez primeras etiquetas
 		for(int i = 0; i < 10; i++)
 			System.out.println("Etiqueta " + i + ": " + String.valueOf(labels[i]));
 		*/
-		
+
 		Entrenador entrenador = new Entrenador();
-		
+
 		System.out.println("Comienza el entrenamiento....");
-		entrenador.ComenzarEntrenamiento("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\train-images-idx3-ubyte.gz", 
+		entrenador.ComenzarEntrenamiento("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\train-images-idx3-ubyte.gz",
 										 "C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\train-labels-idx1-ubyte.gz");
-		
-			
+
+
 		System.out.println("Comienza el test....");
-		entrenador.ComenzarTest("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\t10k-images-idx3-ubyte.gz", 
+		entrenador.ComenzarTest("C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\t10k-images-idx3-ubyte.gz",
 								"C:\\Users\\atr_9\\eclipse-workspace\\MNIST\\data\\t10k-labels-idx1-ubyte.gz");
 
 	}
--- MNIST/src/practica1/Neurona.java
+++ MNIST/src/practica1/Neurona.java
@@ -1,9 +1,9 @@
 package practica1;
 
 /**
- * Clase Neurona encargada de recibir las entradas de la im·gen y activarse
- * si se corresponde al n˙mero encargado de reconocer
- * @author Adri·n
+ * Clase Neurona encargada de recibir las entradas de la im√°gen y activarse
+ * si se corresponde al n√∫mero encargado de reconocer
+ * @author Adri√°n
  *
  */
 public class Neurona {
@@ -14,29 +14,29 @@ public class Neurona {
 	private int nombre;
 	private final int TAM_ENTRADAS = 28*28 +1;
 	private final int TAM_MATRIZ = 28;
-	
+
 	/**
 	 * Constructor
 	 * @param nombre Nombre de la neurona
 	 * @param umbral umbral para la entrada cero
 	 */
 	public Neurona(int nombre, double umbral) {
-		
+
 		pesosEntrada = new double [TAM_ENTRADAS];
-		
+
 		//Valor del umbral
 		pesosEntrada[0] = umbral;
-		
+
 		//Actualizamos los valores de los pesos de forma aleatoria
 		for(int i = 1; i < TAM_ENTRADAS; i++) {
 			pesosEntrada[i] = (float) (Math.random() * 100) + 1;
 		}
-		
+
 		activada = false;
 		this.nombre = nombre;
-		
+
 	}
-	
+
 	/**
 	 * Obtiene el nombre de la neurona
 	 * @return nombre: String
@@ -44,53 +44,53 @@ public class Neurona {
 	int getNombre() {
 		return nombre;
 	}
-	
+
 	boolean getActivada() {
 		return activada;
 	}
-	
+
 	double getPeso ( int entrada ) {
-		
+
 		if(entrada >= 1 && entrada < TAM_ENTRADAS) {
 			return pesosEntrada[entrada];
 		}
 		else
 			return Float.MAX_VALUE;
-		
+
 	}
-	
+
 	void setPeso ( int entrada, float valor ) {
-		
+
 		if(entrada >= 1 && entrada < TAM_ENTRADAS) {
 			pesosEntrada[entrada] = valor;
 		}
-		
+
 	}
-	
+
 	void sumaPeso ( int entrada, double valor ) {
-		
+
 		if(entrada >= 1 && entrada < TAM_ENTRADAS) {
 			pesosEntrada[entrada] = pesosEntrada[entrada] + valor;
 		}
-		
+
 	}
-	
+
 	double [] getPesos() {
 		return pesosEntrada;
 	}
-	
+
 	void setPesos(double new_pesos[]) {
 		if(new_pesos.length == TAM_ENTRADAS - 1)
 			for(int i = 0; i < TAM_ENTRADAS; i++)
 				pesosEntrada[i+1]=new_pesos[i];
 	}
-	
+
 	void variaPesos( int valor[][],boolean suma) {
-		
+
 		int factor = (suma)? 1 : -1;
-		
+
 		if(valor.length == TAM_MATRIZ) {
-			
+
 			int indice = 1;
 			for(int i = 0; i < TAM_MATRIZ; i++) {
 				for(int j = 0; j < TAM_MATRIZ; j++) {
@@ -99,26 +99,26 @@ public class Neurona {
 				}
 			}
 		}
-				
+
 	}
-	
+
 	boolean Evalua(int[][]entrada) {
-		
+
 		double sum = pesosEntrada[0];
 		int indice = 1;
-		
+
 		for(int i = 0; i < TAM_MATRIZ; i++) {
 			for(int j = 0; j < TAM_MATRIZ; j++) {
 				sum += entrada[i][j] * pesosEntrada[indice];
 				indice ++;
 			}
 		}
-		
+
 		activada = (sum>0)? true : false;
-		//System.out.println("EvaluaciÛn de la neurona " + String.valueOf(nombre) + " = " + String.valueOf(sum));
+		//System.out.println("Evaluaci√≥n de la neurona " + String.valueOf(nombre) + " = " + String.valueOf(sum));
 		return activada;
 	}
-	
-	
-	
+
+
+
 }
